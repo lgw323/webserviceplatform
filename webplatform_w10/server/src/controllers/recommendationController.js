a2afa08986690196e53939c2919423e88a860cc6
lgw323 <dlrjsdn0518@gmail.com>
2026-06-05 11:21:44 +0900

    feat: 타겟팅 광고 응답용 API 컨트롤러(getAds) 추가

--- webplatform_w10/server/src/controllers/recommendationController.js
+++ webplatform_w10/server/src/controllers/recommendationController.js
@@ -30,3 +30,50 @@ export const getRecommendations = async (req, res, next) => {
     next(err);
   }
 };
+
+import { detectBottleneck } from '../utils/detectBottleneck.js';
+
+export const getAds = async (req, res, next) => {
+  try {
+    // In reality, hardware info should come from req.user or req.query
+    // For TDD, let's assume it comes from req.user.hardware
+    const hardware = req.user?.hardware || { cpu: 'i5-13600K', gpu: 'RTX 3060', ram: '16GB' };
+    
+    const bottleneck = detectBottleneck(hardware);
+    
+    let ad = null;
+    
+    if (bottleneck.component === 'cpu') {
+      ad = {
+        type: 'cpu',
+        title: '성능 병목 해결! CPU 업그레이드 추천',
+        description: bottleneck.reason,
+        link: 'https://example.com/cpu-upgrade',
+        imageUrl: '/images/ads/cpu.png'
+      };
+    } else if (bottleneck.component === 'gpu') {
+      ad = {
+        type: 'gpu',
+        title: '프레임 드랍 해결! 최신 GPU 추천',
+        description: bottleneck.reason,
+        link: 'https://example.com/gpu-upgrade',
+        imageUrl: '/images/ads/gpu.png'
+      };
+    } else {
+      ad = {
+        type: 'general',
+        title: '게이밍 기어 업그레이드 추천',
+        description: '더 나은 게이밍 환경을 위한 모니터/키보드를 확인해보세요.',
+        link: 'https://example.com/gear',
+        imageUrl: '/images/ads/gear.png'
+      };
+    }
+
+    res.json({
+      success: true,
+      ad
+    });
+  } catch (err) {
+    next(err);
+  }
+};
