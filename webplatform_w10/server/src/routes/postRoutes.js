ade38354ba12ea947fb21cfae44bfd03c221dc76
lgw323 <dlrjsdn0518@gmail.com>
2026-06-11 20:15:03 +0900

    feat(routes): postRoutes에 PUT(수정), DELETE(삭제), POST(좋아요), DELETE(댓글삭제) 라우트 추가

--- webplatform_w10/server/src/routes/postRoutes.js
+++ webplatform_w10/server/src/routes/postRoutes.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import { getPosts, getPostById, createPost, createComment } from '../controllers/postController.js';
+import { getPosts, getPostById, createPost, updatePost, deletePost, toggleLike, createComment, deleteComment } from '../controllers/postController.js';
 import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+// 공개 라우트
 router.get('/', getPosts);
 router.get('/:id', getPostById);
+
+// 인증 필수 라우트
 router.post('/', authenticateToken, createPost);
+router.put('/:id', authenticateToken, updatePost);
+router.delete('/:id', authenticateToken, deletePost);
+router.post('/:id/like', authenticateToken, toggleLike);
 router.post('/:id/comments', authenticateToken, createComment);
+router.delete('/comments/:commentId', authenticateToken, deleteComment);
 
 export default router;
