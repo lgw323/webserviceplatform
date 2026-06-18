da93e0feb387486248d3a2b7834cb7ab0cf9d5b3
이건우 <dlrjsdn0518@gmail.com>
2026-06-18 21:52:21 +0900

    feat: 유저가 작성한 글 및 댓글 조회 API 경로 추가

--- webplatform_w10/server/src/routes/userRoutes.js
+++ webplatform_w10/server/src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { updateNickname } from '../controllers/authController.js';
+import { updateNickname, getUserPosts, getUserComments } from '../controllers/authController.js';
 import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.patch('/me/nickname', authenticateToken, updateNickname);
+router.get('/me/posts', authenticateToken, getUserPosts);
+router.get('/me/comments', authenticateToken, getUserComments);
 
 export default router;
+
