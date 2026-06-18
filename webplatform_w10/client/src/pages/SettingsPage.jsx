e426e67b39bd72dae7f229933eb206687f63c947
이건우 <dlrjsdn0518@gmail.com>
2026-06-18 21:54:02 +0900

    feat: 설정 페이지에서 닉네임 수정 관련 로직 및 의존성 제거

--- webplatform_w10/client/src/pages/SettingsPage.jsx
+++ webplatform_w10/client/src/pages/SettingsPage.jsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import SettingsView from '../features/settings/SettingsView';
-import useAuthStore from '../store/useAuthStore';
 import useSEO from '../hooks/useSEO';
-import * as api from '../api/apiClient';
 
 export default function SettingsPage() {
   useSEO('settings');
-  const { user, setUser } = useAuthStore();
-
-  const handleUpdateNickname = async (name) => {
-    try {
-      const data = await api.updateNickname(name);
-      api.setToken(data.access_token);
-      if (data.refresh_token) {
-        api.setRefreshToken(data.refresh_token);
-      }
-      setUser(data.user);
-    } catch (err) {
-      console.error('Failed to update nickname:', err);
-      throw err;
-    }
-  };
 
   return (
     <section className="animation-fade-in" aria-labelledby="heading-settings">
       <h1 id="heading-settings" className="sr-only">환경 설정</h1>
-      <SettingsView user={user} onUpdateNickname={handleUpdateNickname} />
+      <SettingsView />
     </section>
   );
 }
+
